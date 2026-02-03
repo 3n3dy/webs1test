004c3db7ce4835460437b94db4f07c8ffdc4e51a
feel <chatgptdavaj@gmail.com>
2026-02-03 11:47:14 +0200

    Update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -18,7 +18,7 @@ const Header = memo(() => {
           <img
             alt="Лого ЗВИЧАЇКА"
             className="w-full h-full object-contain"
-            src="/webs1test/favi1.png"
+            src="/public/favi1.png"
           />
         </button>
 
