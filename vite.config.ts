735ba72d56062170dac52a8e406f48491694e67b
feel <chatgptdavaj@gmail.com>
2026-02-04 12:39:07 +0200

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://zvychaika.com',
+      dynamicRoutes: [
+        '/',
+      ],
+      changefreq: 'weekly',
+      priority: 1.0,
+      lastmod: new Date(),
+      readable: true,
+      exclude: ['/404']
+    })
+  ],
   css: {
     postcss: './postcss.config.js',
   },
