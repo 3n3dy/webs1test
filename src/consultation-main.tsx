9508527162ced6ef42a6c74a169ed32f854f0dff
feel <chatgptdavaj@gmail.com>
2026-02-12 17:48:10 +0200

    Update

--- src/consultation-main.tsx
+++ src/consultation-main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './App.css'
+import './index.css'
 import ContactModal from './components/ContactModal'
 
 const ConsultationPage = () => {
